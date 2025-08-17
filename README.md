84e0ff1072ad8386683592047b921508f501f282
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2025-08-17 00:40:02 -0400

    initial transcript maker python script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started
+```sh
+brew install ffmpeg
+pip install -U openai-whisper --break-system-packages
+```
---
+++ main.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import glob 
+import os
+import whisper
+import re
+import warnings
+
+load_dotenv()
+AUDIO_IN_DIR = os.getenv("AUDIO_IN_DIR")
+TEXT_OUT_DIR = os.getenv("TEXT_OUT_DIR")
+warnings.filterwarnings("ignore")
+model = whisper.load_model("medium.en")
+
+audio_paths = glob.glob(f"{AUDIO_IN_DIR}/*.MP3") + glob.glob(f"{AUDIO_IN_DIR}/*.mp3")
+text_paths = glob.glob(f"{TEXT_OUT_DIR}/*.md")
+for audio_path in audio_paths:
+    audio_filename = re.match(r"[a-zA-Z0-9-_\/\\]*[\/\\]([a-zA-Z0-9-_\/\\]*)\.(mp3|MP3)", audio_path).group(1)
+    text_file_exists = False
+    for text_path in text_paths:
+        if audio_filename in text_path:
+            text_file_exists = True
+    if not text_file_exists:
+        print(f"Generating transcript for: {audio_filename}.MP3")
+        result = model.transcribe(audio_path)
+        with open(f"{TEXT_OUT_DIR}/{audio_filename}.md", "w") as file:
+            output_text = result["text"].strip()
+            file.write(output_text)
+print("\nDone!")
